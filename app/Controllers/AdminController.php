c9c68779ba395043b35fb923f6d3d0ee65a269f6
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 10:22:28 +0000

    Try to connect if request mthod is post, render login form otherwise

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -30,15 +30,37 @@ class AdminController extends BaseController
 
     public function login()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Les données de connexion ont été soumises en POST
+
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $user = User::authenticate($email, $password);
+
+            if ($user) {
+                // La connexion a réussi, enregistrer l'utilisateur dans une variable de session
+                $_SESSION['user_id'] = $user->getId();
+
+                // Rediriger vers le tableau de bord
+                $this->redirect('/admin/dashboard');
+            } else {
+                // La connexion a échoué, afficher un message d'erreur
+                $errorMessage = 'Adresse e-mail ou mot de passe incorrect.';
+            }
+        }
+
         // Afficher le formulaire de connexion
-        echo 'Login';
+        $this->renderView('admin/login', [
+            'errorMessage' => $errorMessage ?? null,
+        ]);
     }
 
     public function logout()
     {
         // Détruire la session et rediriger vers la page de connexion
         session_destroy();
-        $this->redirect('/admin/login');    
+        $this->redirect('/admin/login');
     }
 
     private function isLoggedIn()
@@ -55,5 +77,4 @@ class AdminController extends BaseController
 
         return false;
     }
-
 }
