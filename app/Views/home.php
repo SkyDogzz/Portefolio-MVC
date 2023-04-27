7379575fbf5f227dbd62d384d1414868bf32a38b
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 12:51:29 +0000

    create home view

---
+++ app/Views/home.php
@@ -0,0 +1,8 @@
+<h1>Mon CV</h1>
+<a href="public/images/Thomas_STEPHAN_Resume.png" target="_blank">
+    <img src="public/images/Thomas_STEPHAN_Resume.png" alt="CV Thomas Stephan">
+</a>
+<div>
+    <button>Téléchargeren format png</button>
+    <button>Télécharger en format pdf</button>
+</div>
