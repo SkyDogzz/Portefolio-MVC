7773137350c8de12b84f0fc774b0ee83021c1815
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 10:19:42 +0000

    change index, create dashboard, create login, create logout, create isLoggedIn

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -8,9 +8,52 @@ class AdminController extends BaseController
 {
     public function index()
     {
-        $user = new User();
-        
-        echo 'Admin controller';
+        if (!$this->isLoggedIn()) {
+            // Rediriger vers la page de connexion
+            $this->redirect('/admin/login');
+        }
+
+        // Rediriger vers le tableau de bord
+        $this->redirect('/admin/dashboard');
+    }
+
+    public function dashboard()
+    {
+        if (!$this->isLoggedIn()) {
+            // Rediriger vers la page de connexion
+            $this->redirect('/admin/login');
+        }
+
+        // Afficher le tableau de bord
+        echo 'Dashboard';
+    }
+
+    public function login()
+    {
+        // Afficher le formulaire de connexion
+        echo 'Login';
+    }
+
+    public function logout()
+    {
+        // Détruire la session et rediriger vers la page de connexion
+        session_destroy();
+        $this->redirect('/admin/login');    
+    }
+
+    private function isLoggedIn()
+    {
+        // Vérifier si l'utilisateur est connecté
+        if (isset($_SESSION['user_id'])) {
+            $user_id = $_SESSION['user_id'];
+            $user = User::find($user_id);
+
+            if ($user) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
