c3f6ab5b5ddb8a7f5df33ea7f8be52948799efad
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 14:17:03 +0000

    Change button by link

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,7 +2,7 @@
 <a href="public/images/Thomas_STEPHAN_Resume.png" target="_blank">
     <img src="public/images/Thomas_STEPHAN_Resume.png" alt="CV Thomas Stephan">
 </a>
-<div>
-    <button>Téléchargeren format png</button>
-    <button>Télécharger en format pdf</button>
-</div>
+<p>
+    <a href="public/images/Thomas_STEPHAN_Resume.png" download>Télécharger en format png</a>
+    <a href="public/pdf/Thomas_STEPHAN_Resume.pdf" download>Télécharger en format pdf</a>
+</p>
