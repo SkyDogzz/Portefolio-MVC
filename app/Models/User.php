81714d5b8bf7795be0292265ab2de5ea0ad8d7dc
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 10:41:23 +0000

    create authenticate method and findOneByEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,36 @@
 
 namespace App\Models;
 
+use PDO;
+
 class User extends DBModel
 {
     protected $table = 'users';
+
+    public function authenticate($email, $password)
+    {
+        $user = $this->findOneByEmail($email);
+
+        if (!$user) {
+            return false;
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return false;
+        }
+
+        return $user;   
+    }
+
+    private function findOneByEmail($email)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM $this->table WHERE email = :email");
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$result) {
+            return false;
+        }
+        return $result;
+    }
 }
