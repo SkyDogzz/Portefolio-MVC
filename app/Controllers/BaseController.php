860566689e5ff14269df865ebdf5433b7119b7b4
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 09:45:45 +0000

    Create BaseController that will contain method thats used in every controllers

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+class BaseController
+{
+    protected function renderView(string $viewName, array $viewData = [])
+    {
+        $viewFilePath = __DIR__ . '/../Views/' . $viewName . '.php';
+        if (!file_exists($viewFilePath)) {
+            throw new \Exception('View file not found: ' . $viewName);
+        }
+
+        extract($viewData);
+
+        ob_start();
+        require $viewFilePath;
+        $content = ob_get_clean();
+
+        require __DIR__ . '/../Views/layout.php';
+    }
+}
