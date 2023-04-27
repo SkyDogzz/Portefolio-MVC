5f7d888fd7ef6f5ea1ca21625c7475fc0010aaf1
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 12:41:29 +0000

    create save method

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,8 +6,18 @@ use PDO;
 
 class Contact extends DBModel
 {
+    protected $table = 'contact';
 
-    function index()
+    public function save(array $data)
     {
+        $stmt = $this->db->prepare(
+            "INSERT INTO $this->table (subject, message, email) VALUES (:subject, :message, :email)"
+        );
+
+        $stmt->bindParam(':subject', $data['subject'], PDO::PARAM_STR);
+        $stmt->bindParam(':message', $data['message'], PDO::PARAM_STR);
+        $stmt->bindParam(':email', $data['email'], PDO::PARAM_STR);
+
+        return $stmt->execute();
     }
 }
