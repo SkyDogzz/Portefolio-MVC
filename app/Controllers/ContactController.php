385e4642b50ad8f763ed0a6864f3d68907b240db
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 12:20:07 +0000

    create contactcontroller

---
+++ app/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+class ContactController extends BaseController
+{
+
+    public function index()
+
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Les données de formulaire ont été soumises en POST
+
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+
+            // TODO: Traitement et enregistrement du message de contact
+
+            // Rediriger vers la page de confirmation
+            $this->redirect('/contact/confirmation');
+        }
+
+        // Afficher le formulaire de contact
+        $this->renderView('contact/index');
+    }
+}
