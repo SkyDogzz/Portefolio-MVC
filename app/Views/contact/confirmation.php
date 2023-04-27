7b9d55c03955c18c308252e34d68d52ed56ffcdb
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 12:30:32 +0000

    create confirmation view

---
+++ app/Views/contact/confirmation.php
@@ -0,0 +1,5 @@
+<h1>Merci pour votre message !</h1>
+
+<p>Nous avons bien reçu votre message et nous vous répondrons dans les plus brefs délais.</p>
+
+<p>En attendant, n'hésitez pas à parcourir notre site pour découvrir nos produits et services.</p>
