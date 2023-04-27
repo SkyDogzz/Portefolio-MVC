57a99e7b3c273e03a24cebd2bdd8a05d84d029d3
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 11:01:33 +0000

    create debug footer

---
+++ app/Views/partials/footer.php
@@ -0,0 +1,36 @@
+<?php $debug = true; ?>
+
+<footer>
+    <?php if (isset($debug) && $debug == true) {
+    ?>
+        <h1>DEBUG</h1>
+        <div>
+            <pre><h2>$_POST</h2><?php var_dump($_POST); ?></pre>
+            <pre><h2>$_GET</h2><?php var_dump($_GET); ?></pre>
+            <pre><h2>$_SESSION</h2><?php var_dump($_SESSION); ?></pre>
+            <pre><h2>$_SERVER</h2><?php var_dump($_SERVER); ?></pre>
+        </div>
+        <style>
+            footer>h1 {
+                text-align: center;
+            }
+
+            footer>div {
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+                flex-wrap: wrap;
+            }
+
+            pre {
+                margin: 10px;
+                padding: 10px;
+                border: 1px solid black;
+                border-radius: 5px;
+            }
+        </style>
+    <?php
+    }
+    ?>
+</footer>
