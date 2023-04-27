b85855be4220587a94326b54cd70f6889a960fce
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 13:02:15 +0000

    create header

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio de Thomas Stephan</title>
 </head>
+
 <body>
-    
+    <header>
+        <div class="logo">
+            <img src="" alt="Photo Thomas Stephan">
+            <p>Thomas Stephan</p>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/about">A propos</a></li>
+                <li><a href="/project">Projets</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
