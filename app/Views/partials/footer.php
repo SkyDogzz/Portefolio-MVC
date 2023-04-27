b0890c85c91057132951eb2923106bf3d5abd7c5
SkyDogzz <thomas.stephan@live.fr>
2023-04-27 12:19:54 +0000

    add contact form

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -37,4 +37,29 @@
     <?php
     }
     ?>
+    <h2>Me contactez</h2>
+    <form action="/contact" method="post">
+    <div>
+        <label for="name">Nom</label>
+        <input type="text" id="name" name="name" required>
+    </div>
+
+    <div>
+        <label for="email">Adresse e-mail</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+
+    <div>
+        <label for="subject">Objet</label>
+        <input type="text" id="subject" name="subject" required>
+    </div>
+
+    <div>
+        <label for="message">Message</label>
+        <textarea id="message" name="message" required></textarea>
+    </div>
+
+    <button type="submit">Envoyer</button>
+</form>
+
 </footer>
