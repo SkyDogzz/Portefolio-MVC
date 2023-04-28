1eb58dbca3caa23d0c9d2fbecc9e7a525d0e043a
SkyDogzz <thomas.stephan@live.fr>
2023-04-28 12:11:11 +0000

    add style for button container

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,7 +2,7 @@
 <a class="view-cv" href="public/images/Thomas_STEPHAN_Resume.png" target="_blank">
     <img src="public/images/Thomas_STEPHAN_Resume.png" alt="CV Thomas Stephan">
 </a>
-<p>
+<div class="button-container">
     <a class="button-download" href="public/images/Thomas_STEPHAN_Resume.png" download>Télécharger en format png</a>
     <a class="button-download" href="public/pdf/Thomas_STEPHAN_Resume.pdf" download>Télécharger en format pdf</a>
-</p>
+</div>
