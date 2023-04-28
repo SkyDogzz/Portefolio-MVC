075cfe938e06dcffbf3f2e8fff1fffcc7bf0aeea
SkyDogzz <thomas.stephan@live.fr>
2023-04-28 12:38:12 +0000

    give description

--- app/Views/about/index.php
+++ app/Views/about/index.php
@@ -1,1 +1,23 @@
 <h1>A propos</h1>
+
+
+<div class="description-container">
+    <p>
+        Bonjour et bienvenue sur mon portfolio en ligne !
+    </p>
+    <p>
+        Je m'appelle Thomas Stephan et je suis un étudiant en informatique passionné par les nouvelles technologies et leur application pratique. Je suis actuellement en troisième année de Bachelor à l'ESGI (École Supérieure de Génie Informatique) à Paris, en France.
+    </p>
+    <p>
+        J'ai récemment effectué des stages en tant que développeur web chez Fedhubs et BladeCard, où j'ai pu acquérir une expérience précieuse dans le développement de sites web et d'applications. Actuellement, j'exerce le poste d'apprenti en support IT chez OneConciergerie.
+    </p>
+    <p>
+        Je suis également en train de perfectionner mes compétences en informatique en poursuivant mes études en Master d'Ingénierie du Web à l'ESGI, à partir de septembre 2023. Je suis impatient de continuer à me développer en tant que professionnel de l'informatique et de contribuer au monde de la technologie.
+    </p>
+    <p>
+        En termes de compétences, je suis compétent en PHP, HTML, CSS, Javascript et SQL, ainsi qu'en ReactJS et Laravel. Je suis également capable de travailler avec des outils tels que Git et de concevoir des modèles conceptuels de données (MCD / MLD). De plus, je parle couramment l'anglais.
+    </p>
+    <p>
+        En dehors de l'informatique, j'aime jouer à des jeux vidéo, lire des séries de livres et découvrir de nouvelles choses. J'espère que vous apprécierez mon portfolio et que vous trouverez les informations que vous cherchez. N'hésitez pas à me contacter si vous avez des questions ou si vous souhaitez discuter de mes projets.
+    </p>
+</div>
