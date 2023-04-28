8e22ebf2164e7d950d86a8f061a87b6b6b516e0f
SkyDogzz <thomas.stephan@live.fr>
2023-04-28 08:12:57 +0000

    add class to add style

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,6 +3,6 @@
     <img src="public/images/Thomas_STEPHAN_Resume.png" alt="CV Thomas Stephan">
 </a>
 <p>
-    <a href="public/images/Thomas_STEPHAN_Resume.png" download>Télécharger en format png</a>
-    <a href="public/pdf/Thomas_STEPHAN_Resume.pdf" download>Télécharger en format pdf</a>
+    <a class="button-download" href="public/images/Thomas_STEPHAN_Resume.png" download>Télécharger en format png</a>
+    <a class="button-download" href="public/pdf/Thomas_STEPHAN_Resume.pdf" download>Télécharger en format pdf</a>
 </p>
